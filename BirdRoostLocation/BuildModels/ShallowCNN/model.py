bba46ee139560ed03bef9559f4015c890f8b7dbd
Kate Avery <kteavery@gmail.com>
2019-09-10 13:34:47 -0500

    :pencil2::rotating_lights:fix coordconv typos and suppress warning

--- BirdRoostLocation/BuildModels/ShallowCNN/model.py
+++ BirdRoostLocation/BuildModels/ShallowCNN/model.py
@@ -6,14 +6,14 @@ from BirdRoostLocation.BuildModels.ShallowCNN.coord import CoordinateChannel2D
 import keras
 
 
-def build_model(inputDimensions, lr=0.0001, coordConv=False, problem="detection"):
+def build_model(inputDimensions, lr=0.0001, coord_conv=False, problem="detection"):
     """Build the shallow CNN model.
 
     Args:
         inputDimensions: The image dimention input must be in the following
             format: (img_width, img_height, channel)
         lr: The learning rate of the network.
-        coordConv: Adds CoordConv layers
+        coord_conv: Adds CoordConv layers
 
     Returns:
         The shallow CNN model.
@@ -23,7 +23,7 @@ def build_model(inputDimensions, lr=0.0001, coordConv=False, problem="detection"
 
     model = Sequential()
 
-    if coordConv == True:
+    if coord_conv == True:
         model.add(CoordinateChannel2D(input_shape=inputDimensions))
         model.add(Conv2D(8, kernel_size=(5, 5)))
     else:
@@ -33,28 +33,28 @@ def build_model(inputDimensions, lr=0.0001, coordConv=False, problem="detection"
     model.add(Activation("relu"))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-    if coordConv == True:
+    if coord_conv == True:
         model.add(CoordinateChannel2D(use_radius=True))
     model.add(Conv2D(8, (5, 5)))
     model.add(BatchNormalization())
     model.add(Activation("relu"))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-    if coordConv == True:
+    if coord_conv == True:
         model.add(CoordinateChannel2D(use_radius=True))
     model.add(Conv2D(16, (5, 5)))
     model.add(BatchNormalization())
     model.add(Activation("relu"))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-    if coordConv == True:
+    if coord_conv == True:
         model.add(CoordinateChannel2D(use_radius=True))
     model.add(Conv2D(32, (5, 5)))
     model.add(BatchNormalization())
     model.add(Activation("relu"))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-    if coordConv == True:
+    if coord_conv == True:
         model.add(CoordinateChannel2D(use_radius=True))
     model.add(Conv2D(64, (5, 5)))
     model.add(BatchNormalization())
