07002c10f42227f86c10395c54eee4ac249d8d1d
Kate Avery <kteavery@gmail.com>
2019-10-27 14:51:38 -0500

    :construction:same as before

--- BirdRoostLocation/ReadData/BatchGenerator.py
+++ BirdRoostLocation/ReadData/BatchGenerator.py
@@ -278,7 +278,7 @@ class Single_Product_Batch_Generator(Batch_Generator):
             # print(self.label_dict[row["AWS_file"]])
 
     def normalize(self, x, maxi, mini):
-        if list(x):
+        if type(x) is list:
             return [(y - mini) / (maxi - mini) for y in x]
         else:
             return (x - mini) / (maxi - mini)
@@ -379,38 +379,29 @@ class Single_Product_Batch_Generator(Batch_Generator):
                             radii = [polar_radius for i in range(len(images))]
                             thetas = []
                             for i in range(len(images)):
-                                thetas.append(self.adjustTheta(
-                                    polar_theta,
-                                    self.label_dict[filename].images[
-                                        radar_product
-                                    ][i],
-                                ))
+                                thetas.append(
+                                    self.adjustTheta(
+                                        polar_theta,
+                                        self.label_dict[filename].images[radar_product][
+                                            i
+                                        ],
+                                    )
+                                )
+
+                            pairs = list(
+                                    zip(
+                                        self.normalize(radii, 2, 0),
+                                        self.normalize(thetas, 360, 0),
+                                    )
+                                )
+                            pairs = [list(x) for x in pairs]
 
                             if np.array(ground_truths).size == 0:
-                                ground_truths = [
-                                    [
-                                        self.normalize(radii, 2, 0),
-                                        self.normalize(
-                                            thetas,
-                                            360,
-                                            0,
-                                        ),
-                                    ]
-                                ]
+                                ground_truths = pairs
                             else:
                                 ground_truths = np.concatenate(
                                     (
-                                        ground_truths,
-                                        [
-                                            [
-                                                self.normalize(polar_radius, 2, 0),
-                                                self.normalize(
-                                                    polar_theta,
-                                                    360,
-                                                    0,
-                                                ),
-                                            ]
-                                        ],
+                                        ground_truths, pairs,
                                     ),
                                     axis=0,
                                 )
