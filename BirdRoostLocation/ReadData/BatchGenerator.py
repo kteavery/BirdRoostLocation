3fcf806cd899727f975dc14619def6d88846c962
Kate Avery <kteavery@gmail.com>
2020-03-01 15:57:50 -0600

    points in circle print

--- BirdRoostLocation/ReadData/BatchGenerator.py
+++ BirdRoostLocation/ReadData/BatchGenerator.py
@@ -451,6 +451,11 @@ def convert_to_cart(radius, theta):
     return radius * math.cos(theta), radius * math.sin(theta)
 
 def points_in_circle_np(radius, x0=0, y0=0, ):
+    print("x0, y0: ")
+    print(x0 - radius - 1)
+    print(x0 + radius + 1)
+    print(y0 - radius - 1)
+    print(y0 + radius + 1)
     x_ = np.arange(x0 - radius - 1, x0 + radius + 1, dtype=int)
     y_ = np.arange(y0 - radius - 1, y0 + radius + 1, dtype=int)
     x, y = np.where((x_[:,np.newaxis] - x0)**2 + (y_ - y0)**2 <= radius**2)
