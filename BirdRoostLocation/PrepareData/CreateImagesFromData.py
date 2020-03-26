980dcf2699b259b7114a645e3878666bd5c623a1
Kate Avery <kteavery@gmail.com>
2020-03-26 11:57:06 -0500

    remove try

--- BirdRoostLocation/PrepareData/CreateImagesFromData.py
+++ BirdRoostLocation/PrepareData/CreateImagesFromData.py
@@ -40,107 +40,107 @@ def createLabelForFiles(fileNames, saveDir):
 
     radarFilePath = "radarfiles/"
     for f in fileNames:
-        try:
-            # print("FILENAME: ")
-            # print(f)
-            label_row = labelDF.loc[labelDF["AWS_file"] == f]
+        # print("FILENAME: ")
+        # print(f)
+        label_row = labelDF.loc[labelDF["AWS_file"] == f]
 
-            root = os.path.join(radarFilePath, NexradUtils.getBasePath(f))
-            # print("ROOT: ")
-            # print(root)
-            name = f.replace(".gz", "")
-            # print("NAME: ")
-            # print(name)
-            # print("SAVEDIR: ")
-            # print(saveDir)
-            imgDir = os.path.join(saveDir, NexradUtils.getBasePath(f)) + "/"
-            imgPath = os.path.join(
-                imgDir.replace(saveDir, os.path.join(saveDir, "All_Color/")),
-                name + ".png",
+        root = os.path.join(radarFilePath, NexradUtils.getBasePath(f))
+        # print("ROOT: ")
+        # print(root)
+        name = f.replace(".gz", "")
+        # print("NAME: ")
+        # print(name)
+        print("SAVEDIR: ")
+        print(saveDir)
+        imgDir = os.path.join(saveDir, NexradUtils.getBasePath(f)) + "/"
+        imgPath = os.path.join(
+            imgDir.replace(saveDir, os.path.join(saveDir, "All_Color")),
+            name + ".png",
+        )
+        print("IMGPATH:")
+        print(imgPath)
+
+        # print("IF")
+        # print(os.path.isfile(imgPath))
+        # if not os.path.isfile(imgPath):
+
+        file = open(os.path.join(root, name), "r")
+        if not os.path.exists(os.path.dirname(imgPath)):
+            os.makedirs(os.path.dirname(imgPath))
+
+        rad = pyart.io.read_nexrad_archive(file.name)
+
+        dualPol = int(name[-1:]) >= 6
+
+        # VisualizeNexradData.visualizeRadarData(rad, imgPath, dualPol)
+        for i in range(len(label_row)):
+            print("IMGPATH label_row: ")
+            # print(imgPath[:-4] + "_" + str(i) + ".png")
+            lat = float(label_row["lat"].iloc[[i]])
+            lon = float(label_row["lon"].iloc[[i]])
+            print(float(label_row["nexrad_lat"].iloc[[i]]))
+            print(float(label_row["nexrad_lon"].iloc[[i]]))
+            VisualizeNexradData.visualizeRadarData(
+                rad,
+                imgPath[:-4] + "_" + str(i) + ".png",
+                dualPol,
+                nexrads=[
+                    float(label_row["nexrad_lat"].iloc[[i]]),
+                    float(label_row["nexrad_lon"].iloc[[i]]),
+                ],
+                points=[[lat, lon], [lat, lon]],
             )
-            # print("IMGPATH:")
-            # print(imgPath)
+        file.close()
 
-            # print("IF")
-            # print(os.path.isfile(imgPath))
-            # if not os.path.isfile(imgPath):
+        d1 = imgDir.replace(saveDir, os.path.join(saveDir, "Reflectivity_Color/"))
+        d2 = imgDir.replace(saveDir, os.path.join(saveDir, "Velocity_Color/"))
+        if dualPol:
+            d3 = imgDir.replace(saveDir, os.path.join(saveDir, "Rho_HV_Color/"))
+            d4 = imgDir.replace(saveDir, os.path.join(saveDir, "Zdr_Color/"))
 
-            file = open(os.path.join(root, name), "r")
-            if not os.path.exists(os.path.dirname(imgPath)):
-                os.makedirs(os.path.dirname(imgPath))
+        if not os.path.exists(d1):
+            os.makedirs(d1)
+        if not os.path.exists(d2):
+            os.makedirs(d2)
+        if dualPol:
+            if not os.path.exists(d3):
+                os.makedirs(d3)
+            if not os.path.exists(d4):
+                os.makedirs(d4)
 
-            rad = pyart.io.read_nexrad_archive(file.name)
+        img = Image.open(imgPath)
 
-            dualPol = int(name[-1:]) >= 6
+        save_extension = ".png"
+        if not dualPol:
+            img1 = img.crop((115, 100, 365, 350))
+            print("DIR NAMES: ")
+            print(d1 + name + "_Reflectivity" + save_extension)
+            img1.save(d1 + name + "_Reflectivity" + save_extension)
 
-            # VisualizeNexradData.visualizeRadarData(rad, imgPath, dualPol)
-            for i in range(len(label_row)):
-                print("IMGPATH label_row: ")
-                # print(imgPath[:-4] + "_" + str(i) + ".png")
-                lat = float(label_row["lat"].iloc[[i]])
-                lon = float(label_row["lon"].iloc[[i]])
-                print(float(label_row["nexrad_lat"].iloc[[i]]))
-                print(float(label_row["nexrad_lon"].iloc[[i]]))
-                VisualizeNexradData.visualizeRadarData(
-                    rad,
-                    imgPath[:-4] + "_" + str(i) + ".png",
-                    dualPol,
-                    nexrads=[
-                        float(label_row["nexrad_lat"].iloc[[i]]),
-                        float(label_row["nexrad_lon"].iloc[[i]]),
-                    ],
-                    points=[[lat, lon], [lat, lon]],
-                )
-            file.close()
+            img2 = img.crop((495, 100, 740, 350))
+            print(d2 + name + "_Velocity" + save_extension)
+            img2.save(d2 + name + "_Velocity" + save_extension)
 
-            d1 = imgDir.replace(saveDir, os.path.join(saveDir, "Reflectivity_Color/"))
-            d2 = imgDir.replace(saveDir, os.path.join(saveDir, "Velocity_Color/"))
-            if dualPol:
-                d3 = imgDir.replace(saveDir, os.path.join(saveDir, "Rho_HV_Color/"))
-                d4 = imgDir.replace(saveDir, os.path.join(saveDir, "Zdr_Color/"))
+        if dualPol:
+            img1 = img.crop((115, 140, 365, 390))
+            print("DIR NAMES: ")
+            print(d1 + name + "_Reflectivity" + save_extension)
+            img1.save(d1 + name + "_Reflectivity" + save_extension)
 
-            if not os.path.exists(d1):
-                os.makedirs(d1)
-            if not os.path.exists(d2):
-                os.makedirs(d2)
-            if dualPol:
-                if not os.path.exists(d3):
-                    os.makedirs(d3)
-                if not os.path.exists(d4):
-                    os.makedirs(d4)
+            img2 = img.crop((495, 140, 740, 390))
+            print(d2 + name + "_Velocity" + save_extension)
+            img2.save(d2 + name + "_Velocity" + save_extension)
 
-            img = Image.open(imgPath)
-            save_extension = ".png"
-            if not dualPol:
-                img1 = img.crop((115, 100, 365, 350))
-                print("DIR NAMES: ")
-                print(d1 + name + "_Reflectivity" + save_extension)
-                img1.save(d1 + name + "_Reflectivity" + save_extension)
+            img3 = img.crop((115, 520, 365, 770))
+            print(d3 + name + "_Zdr" + save_extension)
+            img3.save(d3 + name + "_Zdr" + save_extension)
 
-                img2 = img.crop((495, 100, 740, 350))
-                print(d2 + name + "_Velocity" + save_extension)
-                img2.save(d2 + name + "_Velocity" + save_extension)
+            img4 = img.crop((495, 520, 740, 770))
+            print(d4 + name + "_Rho_HV" + save_extension)
+            img4.save(d4 + name + "_Rho_HV" + save_extension)
 
-            if dualPol:
-                img1 = img.crop((115, 140, 365, 390))
-                print("DIR NAMES: ")
-                print(d1 + name + "_Reflectivity" + save_extension)
-                img1.save(d1 + name + "_Reflectivity" + save_extension)
-
-                img2 = img.crop((495, 140, 740, 390))
-                print(d2 + name + "_Velocity" + save_extension)
-                img2.save(d2 + name + "_Velocity" + save_extension)
-
-                img3 = img.crop((115, 520, 365, 770))
-                print(d3 + name + "_Zdr" + save_extension)
-                img3.save(d3 + name + "_Zdr" + save_extension)
-
-                img4 = img.crop((495, 520, 740, 770))
-                print(d4 + name + "_Rho_HV" + save_extension)
-                img4.save(d4 + name + "_Rho_HV" + save_extension)
-
-        except Exception as e:
-            print(f"{imgPath}, {str(e)}")
+        #except Exception as e:
+        #    print(f"{imgPath}, {str(e)}")
 
 
 def main(results):
