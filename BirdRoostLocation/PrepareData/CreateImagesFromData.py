ce636e0bfd2cb722491a034f07fd41516789f7bc
Kate Avery <kteavery@gmail.com>
2020-04-07 12:09:46 -0500

    create images 2019

--- BirdRoostLocation/PrepareData/CreateImagesFromData.py
+++ BirdRoostLocation/PrepareData/CreateImagesFromData.py
@@ -17,9 +17,10 @@ import argparse
 import pyart.io
 from PIL import Image
 import pandas
+import glob
 
 
-def createLabelForFiles(fileNames, saveDir):
+def createLabelForFiles(fileNames, saveDir, radarFilePath):
     """Given a Level 2 NEXRAD radar file, create images.
 
     This is a slightly fast and lazy was of creating these images. There is
@@ -34,18 +35,11 @@ def createLabelForFiles(fileNames, saveDir):
     """
     labelDF = pandas.read_csv(settings.WORKING_DIRECTORY + "/" + settings.LABEL_CSV)
 
-    radarFilePath = "radarfiles/"
     for f in fileNames:
-        # print("FILENAME: ")
-        # print(f)
-        label_row = labelDF.loc[labelDF["AWS_file"] == f]
 
         root = os.path.join(radarFilePath, NexradUtils.getBasePath(f))
-        # print("ROOT: ")
-        # print(root)
         name = f.replace(".gz", "")
-        # print("NAME: ")
-        # print(name)
+
         print("SAVEDIR: ")
         print(saveDir)
         imgDir = os.path.join(saveDir, NexradUtils.getBasePath(f)) + "/"
@@ -63,6 +57,7 @@ def createLabelForFiles(fileNames, saveDir):
 
         dualPol = int(name[-1:]) >= 6
 
+        label_row = labelDF.loc[labelDF["AWS_file"] == f]
         for i in range(len(label_row)):
             lat = float(label_row["lat"].iloc[[i]])
             lon = float(label_row["lon"].iloc[[i]])
@@ -79,64 +74,118 @@ def createLabelForFiles(fileNames, saveDir):
             )
         file.close()
 
-        d1 = imgDir.replace(saveDir, os.path.join(saveDir, "Reflectivity_Color/"))
-        d2 = imgDir.replace(saveDir, os.path.join(saveDir, "Velocity_Color/"))
-        if dualPol:
-            d3 = imgDir.replace(saveDir, os.path.join(saveDir, "Rho_HV_Color/"))
-            d4 = imgDir.replace(saveDir, os.path.join(saveDir, "Zdr_Color/"))
+        saveAndSplitImages(imgDir, saveDir, dualPol, imgPath, name)
 
-        if not os.path.exists(d1):
-            os.makedirs(d1)
-        if not os.path.exists(d2):
-            os.makedirs(d2)
-        if dualPol:
-            if not os.path.exists(d3):
-                os.makedirs(d3)
-            if not os.path.exists(d4):
-                os.makedirs(d4)
 
-        img = Image.open(imgPath)
+def createWithoutCSV(fileNames, saveDir, radarFilePath):
+    for f in fileNames:
+        root = os.path.join(radarFilePath, NexradUtils.getBasePath(f))
+        name = f.replace(".gz", "")
 
-        save_extension = ".png"
-        if not dualPol:
-            img1 = img.crop((115, 100, 365, 350))
-            print("DIR NAMES: ")
-            print(d1 + name + "_Reflectivity" + save_extension)
-            img1.save(d1 + name + "_Reflectivity" + save_extension)
+        print("SAVEDIR: ")
+        print(saveDir)
+        imgDir = os.path.join(saveDir, NexradUtils.getBasePath(f)) + "/"
+        imgPath = os.path.join(
+            imgDir.replace(saveDir, os.path.join(saveDir, "All_Color")), name + ".png"
+        )
+        print("IMGPATH:")
+        print(imgPath)
 
-            img2 = img.crop((495, 100, 740, 350))
-            print(d2 + name + "_Velocity" + save_extension)
-            img2.save(d2 + name + "_Velocity" + save_extension)
+        file = open(os.path.join(root, name), "r")
+        if not os.path.exists(os.path.dirname(imgPath)):
+            os.makedirs(os.path.dirname(imgPath))
 
-        if dualPol:
-            img1 = img.crop((115, 140, 365, 390))
-            print("DIR NAMES: ")
-            print(d1 + name + "_Reflectivity" + save_extension)
-            img1.save(d1 + name + "_Reflectivity" + save_extension)
+        rad = pyart.io.read_nexrad_archive(file.name)
 
-            img2 = img.crop((495, 140, 740, 390))
-            print(d2 + name + "_Velocity" + save_extension)
-            img2.save(d2 + name + "_Velocity" + save_extension)
+        dualPol = int(name[-1:]) >= 6
 
-            img3 = img.crop((115, 520, 365, 770))
-            print(d3 + name + "_Zdr" + save_extension)
-            img3.save(d3 + name + "_Zdr" + save_extension)
+        nexrad_csv = pandas.read_csv(
+            saveDir + "/nexrad.csv", names=["radar", "lat", "lon"]
+        )
+        for i in range(len(fileNames)):
+            nexrad_row = nexrad_csv[nexrad_csv["radar"] == fileNames[i][0:4]]
+            VisualizeNexradData.visualizeRadarData(
+                rad,
+                imgPath[:-4] + "_" + str(i) + ".png",
+                dualPol,
+                nexrads=[
+                    float(nexrad_row["lat"].iloc[[i]]),
+                    float(nexrad_row["lon"].iloc[[i]]),
+                ],
+            )
 
-            img4 = img.crop((495, 520, 740, 770))
-            print(d4 + name + "_Rho_HV" + save_extension)
-            img4.save(d4 + name + "_Rho_HV" + save_extension)
+    saveAndSplitImages(imgDir, saveDir, dualPol, imgPath, name)
+
+
+def saveAndSplitImages(imgDir, saveDir, dualPol, imgPath, name):
+    d1 = imgDir.replace(saveDir, os.path.join(saveDir, "Reflectivity_Color/"))
+    d2 = imgDir.replace(saveDir, os.path.join(saveDir, "Velocity_Color/"))
+    if dualPol:
+        d3 = imgDir.replace(saveDir, os.path.join(saveDir, "Rho_HV_Color/"))
+        d4 = imgDir.replace(saveDir, os.path.join(saveDir, "Zdr_Color/"))
+
+    if not os.path.exists(d1):
+        os.makedirs(d1)
+    if not os.path.exists(d2):
+        os.makedirs(d2)
+    if dualPol:
+        if not os.path.exists(d3):
+            os.makedirs(d3)
+        if not os.path.exists(d4):
+            os.makedirs(d4)
+
+    img = Image.open(imgPath)
+
+    save_extension = ".png"
+    if not dualPol:
+        img1 = img.crop((115, 100, 365, 350))
+        print("DIR NAMES: ")
+        print(d1 + name + "_Reflectivity" + save_extension)
+        img1.save(d1 + name + "_Reflectivity" + save_extension)
+
+        img2 = img.crop((495, 100, 740, 350))
+        print(d2 + name + "_Velocity" + save_extension)
+        img2.save(d2 + name + "_Velocity" + save_extension)
+
+    if dualPol:
+        img1 = img.crop((115, 140, 365, 390))
+        print("DIR NAMES: ")
+        print(d1 + name + "_Reflectivity" + save_extension)
+        img1.save(d1 + name + "_Reflectivity" + save_extension)
+
+        img2 = img.crop((495, 140, 740, 390))
+        print(d2 + name + "_Velocity" + save_extension)
+        img2.save(d2 + name + "_Velocity" + save_extension)
+
+        img3 = img.crop((115, 520, 365, 770))
+        print(d3 + name + "_Zdr" + save_extension)
+        img3.save(d3 + name + "_Zdr" + save_extension)
+
+        img4 = img.crop((495, 520, 740, 770))
+        print(d4 + name + "_Rho_HV" + save_extension)
+        img4.save(d4 + name + "_Rho_HV" + save_extension)
 
 
 def main(results):
     """Formatted to run either locally or on schooner. Read in csv and get radar
      files listed in 'AWS_file' column. Save these files out as png images."""
-    labels = pandas.read_csv(
-        filepath_or_buffer=settings.LABEL_CSV, skip_blank_lines=True
-    )
+    # labels = pandas.read_csv(
+    #     filepath_or_buffer=settings.LABEL_CSV, skip_blank_lines=True
+    # )
 
-    radar_labels = labels[labels.radar == results.radar]
-    createLabelForFiles(
-        fileNames=list(radar_labels["AWS_file"]), saveDir=utils.RADAR_IMAGE_DIR
+    # radar_labels = labels[labels.radar == results.radar]
+    # createLabelForFiles(
+    #     fileNames=list(radar_labels["AWS_file"]),
+    #     saveDir=utils.RADAR_IMAGE_DIR,
+    #     radarFilePath="radarfiles/",
+    # )
+    aws_files = []
+    for file in glob.glob("*.gz"):
+        aws_files.append(file[0:19])
+    createWithoutCSV(
+        fileNames=aws_files,
+        saveDir=utils.RADAR_IMAGE_DIR,
+        radarFilePath="2019radarfiles/",
     )
 
 
