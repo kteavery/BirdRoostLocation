99337e6a8c6220b8fb5e0003da8cf5c73852a5e7
Kate Avery <kteavery@gmail.com>
2020-06-02 14:14:17 -0400

    input shape

--- BirdRoostLocation/ReadData/BatchGenerator.py
+++ BirdRoostLocation/ReadData/BatchGenerator.py
@@ -586,7 +586,7 @@ class Multiple_Product_Batch_Generator(Batch_Generator):
                 # print("train_batch.shape")
                 # print(train_batch.shape)
                 pred = loaded_models[k].predict_proba(train_batch)
-                predictions = np.append(predictions, np.array([pred, 1 - pred]))
+                predictions = np.append(predictions, np.array([pred, 1 - pred]), axis=1)
 
             print(len(predictions))
             print(type(predictions))
