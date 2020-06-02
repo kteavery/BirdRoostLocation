4254f970d2bc5b99958987524a90719d43a71b54
Kate Avery <kteavery@gmail.com>
2020-06-02 16:28:32 -0400

    input shape

--- BirdRoostLocation/BuildModels/ShallowCNN/train.py
+++ BirdRoostLocation/BuildModels/ShallowCNN/train.py
@@ -205,7 +205,7 @@ def train(
             )
         else:
             model = Sequential()
-            model.add(Dense(16, input_shape=(4, 2), activation="relu"))
+            model.add(Dense(16, input_shape=(4, 4), activation="relu"))
             model.add(Dense(2, activation="softmax"))
             model.compile(
                 loss=keras.losses.categorical_crossentropy,
@@ -260,7 +260,7 @@ def train(
                     num_temporal_data=num_temporal_data,
                 )
                 x = np.reshape(x, (x.shape[1], x.shape[0], x.shape[2], x.shape[3]))
-                print(x)
+                y = np.reshape(y, (y.shape[1], y.shape[0], y.shape[2]))
                 # print(y)
                 # x = np.array(list(x))
                 # print("batch output")
