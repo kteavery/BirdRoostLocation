854537fd4c6fb81849ffa3bc864181bc8886c69d
Kate Avery <kteavery@gmail.com>
2020-06-12 17:33:59 -0400

    roc curve

--- BirdRoostLocation/BuildModels/ShallowCNN/train.py
+++ BirdRoostLocation/BuildModels/ShallowCNN/train.py
@@ -198,6 +198,8 @@ def train(
                     model_type=model_type,
                     problem=problem,
                 )
+                print(x)
+                print(y)
                 if type(x) != type(None) and type(y) != type(None):
                     print("X and Y shapes")
                     print(x.shape)
