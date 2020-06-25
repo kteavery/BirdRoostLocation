3d370fca1e39d669554c93af1c43177cf9bcd493
Kate Avery <kteavery@gmail.com>
2020-06-25 11:44:03 -0400

    prints

--- BirdRoostLocation/BuildModels/ShallowCNN/eval.py
+++ BirdRoostLocation/BuildModels/ShallowCNN/eval.py
@@ -100,11 +100,11 @@ def eval(
             else:
                 field_ys = np.array([])
                 field_preds = np.array([])
+                combined_data = pd.DataFrame()
                 for i, field in enumerate(
                     ["Reflectivity", "Velocity", "Rho_HV", "Zdr"]
                 ):
                     print(field)
-                    print(log_path)
 
                     data = pd.read_csv(
                         settings.WORKING_DIRECTORY
@@ -118,27 +118,43 @@ def eval(
                     print(data.head())
                     filtered_data = data.loc[data["filename"].isin(filenames)]
                     print(filtered_data.head())
-                    y = filtered_data["label"].to_numpy()
-                    preds = filtered_data["prediction"].to_numpy()
 
-                    print(preds.shape)
-                    print(y.shape)
-
-                    print(field_preds.shape)
-                    print(field_ys.shape)
-
-                    print("prelim skill scores")
-                    print(SkillScores.get_skill_scores(preds, y))
-
-                    preds = np.array([np.array([a, 1.0 - a]) for a in preds])
-                    y = np.array([np.array([a, 1.0 - a]) for a in y])
-
-                    if field_preds.size == 0:
-                        field_preds = preds
-                        field_ys = y
+                    if combined_data.empty:
+                        combined_data = filtered_data
                     else:
-                        field_preds = np.concatenate((field_preds, preds))
-                        field_ys = np.concatenate((field_ys, y))
+                        combined_data.join(
+                            filtered_data, on="filename", how="inner", rsuffix=field
+                        )
+
+                    print(combined_data.head())
+
+                    # y = filtered_data["label"].to_numpy()
+                    # preds = filtered_data["prediction"].to_numpy()
+
+                    # print(preds.shape)
+                    # print(y.shape)
+
+                    # print(field_preds.shape)
+                    # print(field_ys.shape)
+
+                    # print("prelim skill scores")
+                    # print(SkillScores.get_skill_scores(preds, y))
+
+                    # preds = np.array([np.array([a, 1.0 - a]) for a in preds])
+                    # y = np.array([np.array([a, 1.0 - a]) for a in y])
+                    # print("preds, y")
+                    # print(preds.shape)
+                    # print(y.shape)
+
+                    # if field_preds.size == 0:
+                    #     field_preds = preds
+                    #     field_ys = y
+                    # else:
+                    #     field_preds = np.concatenate((field_preds, preds), axis=0)
+                    #     field_ys = np.concatenate((field_ys, y), axis=0)
+
+                field_ys = combined_data["label"].to_numpy()
+                field_preds = combined_data.filter(regex="prediction").to_numpy()
 
                 model = Sequential()
                 model.add(Dense(256, input_shape=(4, 2), activation="relu"))
@@ -152,11 +168,9 @@ def eval(
 
                 print(field_preds.shape)
                 print(field_ys.shape)
-                predictions = model.predict(
-                    np.reshape(field_preds, (preds.shape[0], 4, 2))
-                )
+                predictions = model.predict(np.reshape(field_preds, (-1, 4, 2)))
                 # predictions = np.reshape(field_preds, (preds.shape[0], 4, 4))
-                y = np.reshape(field_ys, (preds.shape[0], 4, 2))
+                y = np.reshape(field_ys, (-1, 4, 2))
 
         except AttributeError as e:
             print(e)
